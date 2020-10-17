c067db13048a1be19fec7b75b4ad7890a5dacc46
bs2kbs2k <bs2k@naver.com>
2020-10-17 18:44:30 +0900

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,10 +2,9 @@ from PyQt5.QtWidgets import QApplication
 import PyQt5.QtWidgets as qw
 import PyQt5.uic as uic
 import sys
-import os
 import os.path
 app = QApplication(sys.argv)
-ui = uic.loadUi(os.path.join(os.getenv('SMLINUX'),'sml.ui'))
+ui = uic.loadUi(os.path.join(input(),'sml.ui'))
 ui.show()
 def getStatus(el):
     if isinstance(el, qw.QComboBox):
